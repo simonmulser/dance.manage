9544ac21ed6b4d58b433ec7604da87156f883636
Ewald Niederstaetter <e0926036@student.tuwien.ac.at>
2013-12-08 14:54:00 +0100

    Implementation of invoiceview (1.1) refs #26559

---
+++ src/main/java/at/danceandfun/controller/InvoiceController.java
@@ -0,0 +1,10 @@
+package at.danceandfun.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "admin/invoice")
+public class InvoiceController {
+
+}
