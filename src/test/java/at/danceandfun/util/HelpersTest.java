5061430262006861ee88657732a194a6bfda4890
Sebastian Haas <e0925299@student.tuwien.ac.at>
2013-12-10 16:07:21 +0100

    Unittest komplettiert
    refs #26417 @0.2

--- src/test/java/at/danceandfun/util/HelpersTest.java
+++ src/test/java/at/danceandfun/util/HelpersTest.java
@@ -11,41 +11,64 @@ import at.danceandfun.entity.Course;
 
 public class HelpersTest {
 
-	@Test
-	public void testGetCurrentWeekWeekdayRepresentation() {
-		LocalDate today = new LocalDate();
-		LocalDate result = Helpers.getCurrentWeekWeekdayRepresentation(today
-				.getDayOfWeek());
-		assertEquals(today, result);
-	}
+    @Test
+    public void testGetCurrentWeekWeekdayRepresentation() {
+        LocalDate today = new LocalDate();
+        LocalDate result = Helpers.getCurrentWeekWeekdayRepresentation(today
+                .getDayOfWeek());
+        assertEquals(today, result);
+    }
 
-	@Test
-	public void testGetCourseStartDateTimeCurrentWeekRepresentation() {
-		Course course = CourseDaoTest.getValidCourse();
-		DateTime result = Helpers
-				.getCourseStartDateTimeCurrentWeekRepresentation(course);
-		assertEquals(course.getTime().getHourOfDay(), result.getHourOfDay());
-		assertEquals(course.getTime().getMinuteOfHour(),
-				result.getMinuteOfHour());
-		assertEquals(course.getTime().getSecondOfMinute(),
-				result.getSecondOfMinute());
-		assertEquals(course.getTime().getMillisOfSecond(),
-				result.getMillisOfSecond());
-		assertEquals((int) course.getWeekday().getValue() + 1,
-				result.getDayOfWeek());
+    @Test
+    public void testGetCourseStartDateTimeCurrentWeekRepresentation() {
+        Course course = CourseDaoTest.getValidCourse();
+        DateTime result = Helpers
+                .getCourseStartDateTimeCurrentWeekRepresentation(course);
+        assertEquals(course.getTime().getHourOfDay(), result.getHourOfDay());
+        assertEquals(course.getTime().getMinuteOfHour(),
+                result.getMinuteOfHour());
+        assertEquals(course.getTime().getSecondOfMinute(),
+                result.getSecondOfMinute());
+        assertEquals(course.getTime().getMillisOfSecond(),
+                result.getMillisOfSecond());
+        assertEquals((int) course.getWeekday().getValue() + 1,
+                result.getDayOfWeek());
 
-		LocalDate now = new LocalDate();
-		LocalDate weekDayOfThisWeek = now.withDayOfWeek(course.getWeekday()
-				.getValue() + 1);
-		assertEquals(weekDayOfThisWeek.getDayOfWeek(), result.getDayOfWeek());
-		assertEquals(weekDayOfThisWeek.getMonthOfYear(),
-				result.getMonthOfYear());
-		assertEquals(weekDayOfThisWeek.getWeekOfWeekyear(),
-				result.getWeekOfWeekyear());
-		assertEquals(weekDayOfThisWeek.getYear(), result.getYear());
-	}
-	
-	@Test
-	public void testGetCourseSEndDateTimeCurrentWeekRepresentation() {	    
-	}
+        LocalDate now = new LocalDate();
+        LocalDate weekDayOfThisWeek = now.withDayOfWeek(course.getWeekday()
+                .getValue() + 1);
+        assertEquals(weekDayOfThisWeek.getDayOfWeek(), result.getDayOfWeek());
+        assertEquals(weekDayOfThisWeek.getMonthOfYear(),
+                result.getMonthOfYear());
+        assertEquals(weekDayOfThisWeek.getWeekOfWeekyear(),
+                result.getWeekOfWeekyear());
+        assertEquals(weekDayOfThisWeek.getYear(), result.getYear());
+    }
+
+    @Test
+    public void testGetCourseSEndDateTimeCurrentWeekRepresentation() {
+        Course course = CourseDaoTest.getValidCourse();
+        int durationMinutes = course.getDuration().getValue();
+        DateTime result = Helpers
+                .getCourseEndDateTimeCurrentWeekRepresentation(course);
+        assertEquals(course.getTime().plusMinutes(durationMinutes).getHourOfDay(), result.getHourOfDay());
+        assertEquals(course.getTime().plusMinutes(durationMinutes).getMinuteOfHour(),
+                result.getMinuteOfHour());
+        assertEquals(course.getTime().plusMinutes(durationMinutes).getSecondOfMinute(),
+                result.getSecondOfMinute());
+        assertEquals(course.getTime().plusMinutes(durationMinutes).getMillisOfSecond(),
+                result.getMillisOfSecond());
+        assertEquals((int) course.getWeekday().getValue() + 1,
+                result.getDayOfWeek());
+
+        LocalDate now = new LocalDate();
+        LocalDate weekDayOfThisWeek = now.withDayOfWeek(course.getWeekday()
+                .getValue() + 1);
+        assertEquals(weekDayOfThisWeek.getDayOfWeek(), result.getDayOfWeek());
+        assertEquals(weekDayOfThisWeek.getMonthOfYear(),
+                result.getMonthOfYear());
+        assertEquals(weekDayOfThisWeek.getWeekOfWeekyear(),
+                result.getWeekOfWeekyear());
+        assertEquals(weekDayOfThisWeek.getYear(), result.getYear());
+    }
 }
