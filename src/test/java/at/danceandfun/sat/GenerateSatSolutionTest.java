87fd1fc907ba81d770410f29343c95a055a761d4
Sebastian <pauer.sebastian@gmail.com>
2014-01-16 19:06:22 +0100

    Unit Tests fuer den Solver hinzugefuegt

---
+++ src/test/java/at/danceandfun/sat/GenerateSatSolutionTest.java
@@ -0,0 +1,54 @@
+package at.danceandfun.sat;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+
+import java.io.IOException;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import at.danceandfun.entity.Performance;
+import at.danceandfun.exception.SatException;
+import at.danceandfun.service.CourseManager;
+import at.danceandfun.service.ParticipantManager;
+
+@Transactional
+@ContextConfiguration("classpath:test/test-context.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+public class GenerateSatSolutionTest {
+
+    private static Logger logger = Logger
+            .getLogger(GenerateSatSolutionTest.class);
+
+    @Autowired
+    private CourseManager courseManager;
+    @Autowired
+    private ParticipantManager participantManager;
+
+    @Test
+    public void testGenerateSolution() {
+        GenerateSatSolution solution = new GenerateSatSolution();
+        try {
+            Map<Integer, Performance> performanceMap = solution
+                    .generatePerformance(courseManager.getEnabledList(),
+                            participantManager.getEnabledList(), true, true,
+                            true, true, true, false, false);
+            assertThat(performanceMap.get(1), is(notNullValue()));
+            assertThat(performanceMap.get(2), is(notNullValue()));
+            assertThat(performanceMap.get(3), is(notNullValue()));
+        } catch (IOException e) {
+
+        } catch (SatException e) {
+
+        }
+    }
+
+}
---
+++ src/test/java/at/danceandfun/sat/SatValidatorTest.java
@@ -0,0 +1,118 @@
+package at.danceandfun.sat;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import at.danceandfun.entity.Course;
+import at.danceandfun.entity.Performance;
+import at.danceandfun.entity.PerformancePlan;
+import at.danceandfun.service.CourseManager;
+import at.danceandfun.service.ParticipantManager;
+import at.danceandfun.service.PerformancePlanManager;
+
+@Transactional
+@ContextConfiguration("classpath:test/test-context.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+public class SatValidatorTest {
+
+    private static Logger logger = Logger.getLogger(SatValidatorTest.class);
+
+    @Autowired
+    private PerformancePlanManager performancePlanManager;
+
+    @Autowired
+    private CourseManager courseManager;
+
+    @Autowired
+    private ParticipantManager participantManager;
+
+    private List<Course> fetchedCourses;
+    private List<PerformancePlan> fetchedPerformancePlans;
+    private List<Performance> fetchedPerformances;
+    private PerformancePlan plan;
+    private Map<Integer, Performance> performancePlanMap;
+
+    @Before
+    public void setUp() {
+        fetchedPerformancePlans = performancePlanManager.getEnabledList();
+        fetchedCourses = courseManager.getEnabledList();
+
+        for (PerformancePlan currentPlan : fetchedPerformancePlans) {
+            if (currentPlan.getPlanid() == 1) {
+                plan = currentPlan;
+            }
+        }
+
+        fetchedPerformances = plan.getPerformances();
+
+        for (Performance currentPerformance : fetchedPerformances) {
+            List<Course> courseList = new ArrayList<Course>();
+            for (Integer currentID : currentPerformance.getCourseIds()) {
+                for (Course currrentCourse : fetchedCourses) {
+                    if (currrentCourse.getCid() == currentID) {
+                        courseList.add(currrentCourse);
+                        continue;
+                    }
+                }
+            }
+            currentPerformance.setCourses(courseList);
+        }
+
+        performancePlanMap = new HashMap<Integer, Performance>();
+
+        performancePlanMap.put(1, fetchedPerformances.get(0));
+        performancePlanMap.put(2, fetchedPerformances.get(1));
+        performancePlanMap.put(3, fetchedPerformances.get(2));
+
+        SatValidator validator = new SatValidator(performancePlanMap,
+                participantManager.getEnabledList());
+        performancePlanMap = validator.validatePerformancePlan();
+    }
+
+    @After
+    public void tearDown() {
+        fetchedCourses = new ArrayList<Course>();
+        fetchedPerformancePlans = new ArrayList<PerformancePlan>();
+        fetchedPerformances = new ArrayList<Performance>();
+        plan = new PerformancePlan();
+    }
+
+    @Test
+    public void testValidationOfPerformanceVideoclipdancing2() {
+        Performance currentPerformance = performancePlanMap.get(1);
+
+        for (Course currentCourse : currentPerformance.getCourses()) {
+            if (currentCourse.getName().equals("Videoclipdancing 2")) {
+                assertTrue(currentCourse.getTwoBreaksRestriction());
+                break;
+            }
+        }
+    }
+
+    @Test
+    public void testValidationOfPerformanceBallett1MO() {
+        Performance currentPerformance = performancePlanMap.get(1);
+
+        for (Course currentCourse : currentPerformance.getCourses()) {
+            if (currentCourse.getName().equals("Ballett 1 MO")) {
+                assertFalse(currentCourse.getBalletRestriction());
+                break;
+            }
+        }
+    }
+}
