982b6a1ec44e41638757078799d2068d98dcd414
Simon Mulser <simonmulser@gmail.com>
2014-01-23 18:22:19 +0100

    refs #28403 @1.5h

--- src/main/webapp/js/searchBoxAutoComplete.js
+++ src/main/webapp/js/searchBoxAutoComplete.js
@@ -1,34 +1,34 @@
 $(function () {
 	$(document).ready(
-		$("#searchQuery").autocomplete({
-			
-									minLength : 1,
-									delay : 500,
-									source : function(request, response) {
-										$.getJSON("/dancemanage/admin/search/searchQuery",
-											request,function(result) {
-												response($.map(result,
-													function(item) {
-														return {
-															label : item.name,
-															value : item.name,
-															id : item.id,
-															image : item.icon,
-															oname : item.objectName
-														};
-											}));
-										});
-									},
-
-									select : function(event, ui) {
-										if (ui.item) {
-											window.location.href = '/dancemanage/admin/' + ui.item.oname + '/edit/' + ui.item.id + '#add';
-										}
-									}
-		}).data( "ui-autocomplete" )._renderItem = function( ul, item ) {
-        return $( "<li>" )
-        	.data( "item.autocomplete", item )
-        	.append( "<a> <i class='"+ item.image +"''></i>&nbsp;" + item.label + " </a>" )
-        	.appendTo( ul );
-    });
+			$("#searchQuery").autocomplete({	
+				minLength: 1,
+				delay: 500,
+				source: function(request, response) {
+					$.getJSON("/dancemanage/admin/search/searchQuery",
+							request,function(result) {
+						response($.map(result,
+								function(item) {
+							return {
+								label : item.name,
+								value : item.name,
+								id : item.id,
+								image : item.icon,
+								oname : item.objectName
+							};
+						}));
+					});
+				},
+				select: function(event, ui) {
+					if (ui.item) {
+						window.location.href = '/dancemanage/admin/' + ui.item.oname + '/edit/' + ui.item.id + '#add';
+					}
+				}
+			}).data("autocomplete")._renderItem = function( ul, item ) {
+				if(item){
+					return $("<li>")
+					.data("item.autocomplete", item )
+					.append("<a> <i class='"+ item.image +"''></i>&nbsp;" + item.label + " </a>")
+					.appendTo(ul);
+				}
+			});
 });
